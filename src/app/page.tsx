d81639e75b921b12972c04c1630ada701ccfab34
hsnabdullah <hsnabdullah89@gmail.com>
2023-10-30 16:42:18 +0500

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
- 
+
 export default function Home() {
 
   return (
     <>
-    
       <div className="relative overflow-hidden bg-white">
         <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
