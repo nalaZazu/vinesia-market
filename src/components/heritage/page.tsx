2318c6c66b9a94fb3b72df1f209e43a386c6fb2e
uswanadeem09675 <wtsplayer001@gamil.com>
2023-11-01 14:00:32 +0500

    all-done section

--- src/components/heritage/page.tsx
+++ src/components/heritage/page.tsx
@@ -9,88 +9,97 @@ const Heritage = () => {
       <h1 className="text-primary font-bold">Invest in heritage</h1>
       <div className="mx-auto max-w-7xl px-4 py-8">
         <div className="mx-auto max-w-2xl  lg:max-w-none  ">
-          <div className="mt-6  grid lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-          <div>
-              <Image
-                src={winbottle}
-                alt="Picture of the author"
-                className="mb-3"
-              />
-              <h4 className="w-3/4     font-semibold  text-base	leading-6">
-                Brunello di Montalcino "Piaggione" | 2019
-              </h4>
-              <p className="flex text-xs  items-center font-medium">
-                6 bottles collection{" "}
-                <span className="ml-2">
+          {/* <div className="mt-6  grid lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
+        
+          </div>   */}
+
+          <div className="  grid lg:grid-cols-2 md:grid-cols-1 grid-cols-2 gap-4 md:gap-0">
+            <div className="lg:mb-0 mb-6">
+              <div className="lg:grid lg:grid-cols-2 md:grid-cols-1 grid-cols-2 lg:gap-8 md:gap-0">
+                <div>
                   <Image
-                    src={alert}
+                    src={winbottle}
                     alt="Picture of the author"
-                    width={15}
-                    height={15}
-                    quality={75}
+                    className="mb-3"
                   />
-                </span>
-              </p>
-              <span>
-                <p className="font-semibold text-xs">Owner</p>
-                <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
-                  Vinesia
-                </span>
-              </span>
-              <hr className="my-4 text-[#C6C7C8] font-bold" />
-              <span>
-                <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
-                <h3 className="font-semibold text-lg">€3,600</h3>
-              </span>
-              <span className="flex font-medium text-xs">
-                <p>50 bottles</p>
-                <p className="text-[#959596] ml-3">50 remaining </p>
-              </span>
-            </div>
-            <div>
-              <Image
-                src={winbottle}
-                alt="Picture of the author"
-                className="mb-3"
-              />
-              <h4 className="w-3/4     font-semibold  text-base	leading-6">
-                Brunello di Montalcino "Piaggione" | 2019
-              </h4>
-              <p className="flex text-xs  items-center font-medium">
-                6 bottles collection{" "}
-                <span className="ml-2">
+                  <h4 className=" font-semibold  text-base	leading-6">
+                    Brunello di Montalcino "Piaggione" | 2019
+                  </h4>
+                  <p className="flex text-xs  items-center font-medium">
+                    6 bottles collection{" "}
+                    <span className="ml-2">
+                      <Image
+                        src={alert}
+                        alt="Picture of the author"
+                        width={15}
+                        height={15}
+                        quality={75}
+                      />
+                    </span>
+                  </p>
+                  <span>
+                    <p className="font-semibold text-xs">Owner</p>
+                    <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
+                      Vinesia
+                    </span>
+                  </span>
+                  <hr className="my-4 text-[#C6C7C8] font-bold" />
+                  <span>
+                    <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
+                    <h3 className="font-semibold text-lg">€3,600</h3>
+                  </span>
+                  <span className="flex font-medium text-xs">
+                    <p>50 bottles</p>
+                    <p className="text-[#959596] ml-3">50 remaining </p>
+                  </span>
+                </div>
+                <div>
                   <Image
-                    src={alert}
+                    src={winbottle}
                     alt="Picture of the author"
-                    width={15}
-                    height={15}
-                    quality={75}
+                    className="mb-3"
                   />
-                </span>
-              </p>
-              <span>
-                <p className="font-semibold text-xs">Owner</p>
-                <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
-                  Vinesia
-                </span>
-              </span>
-              <hr className="my-4 text-[#C6C7C8] font-bold" />
-              <span>
-                <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
-                <h3 className="font-semibold text-lg">€3,600</h3>
-              </span>
-              <span className="flex font-medium text-xs">
-                <p>50 bottles</p>
-                <p className="text-[#959596] ml-3">50 remaining </p>
-              </span>
+                  <h4 className=" font-semibold  text-base	leading-6">
+                    Brunello di Montalcino "Piaggione" | 2019
+                  </h4>
+                  <p className="flex text-xs  items-center font-medium">
+                    6 bottles collection{" "}
+                    <span className="ml-2">
+                      <Image
+                        src={alert}
+                        alt="Picture of the author"
+                        width={15}
+                        height={15}
+                        quality={75}
+                      />
+                    </span>
+                  </p>
+                  <span>
+                    <p className="font-semibold text-xs">Owner</p>
+                    <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
+                      Vinesia
+                    </span>
+                  </span>
+                  <hr className="my-4 text-[#C6C7C8] font-bold" />
+                  <span>
+                    <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
+                    <h3 className="font-semibold text-lg">€3,600</h3>
+                  </span>
+                  <span className="flex font-medium text-xs">
+                    <p>50 bottles</p>
+                    <p className="text-[#959596] ml-3">50 remaining </p>
+                  </span>
+                </div>
+              </div>
             </div>
-            <Image
-              src={backbg}
-              alt="Picture of the author"
-              width={200}
-              // height={150}
-              className="mb-3"
-            />
+
+            <div className="ml-4  lg:mt-0">
+              <Image
+                src={backbg}
+                alt="Picture of the author"
+                className="mb-3 md:mb-0 "
+              />
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,14 @@ module.exports = {
     extend: {
       colors: {
         themegray: "#F4F4F4",
-        spacegray: "#41464b",
-        /**market listing color  */
-
-        availableGray: '#D9D9D9',
-        themegray: '#F4F4F4',
-        spacegray: '#41464b',
+        spacegray: "#41464b", 
         typegray: '#6C757D',
-      },
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
-
+        /**market listing color  */
         primary: "#41464B",
         secondary: "#000000",
         bgprimary: "#D9D9D9",
         bgsecondary: "#6C757D",  
+        // #6C757D
       },      
     },
   },
