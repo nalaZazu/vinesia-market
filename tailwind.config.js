c368a7a1e4ec3b9bd1873f9a9e331ee05a422e31
hsnabdullah <hsnabdullah89@gmail.com>
2023-11-01 18:23:21 +0500

    products page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,23 +10,19 @@ module.exports = {
       colors: {
         themegray: "#F4F4F4",
         spacegray: "#41464b",
+        bordergray: '#A0A6AC',
+        tabsgray: '#C6C7C8',
+        
         /**market listing color  */
 
         availableGray: '#D9D9D9',
         themegray: '#F4F4F4',
         spacegray: '#41464b',
         typegray: '#6C757D',
-      },
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
-
         primary: "#41464B",
         secondary: "#000000",
         bgprimary: "#D9D9D9",
-        bgsecondary: "#6C757D",  
+        bgsecondary: "#6C757D",
       },      
     },
   },
