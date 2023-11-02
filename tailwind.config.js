cf0e121eba695e1aa3c381f7327ce634b1461a52
tauqeer180 <tauqeermalik855@gmail.com>
2023-11-02 12:40:10 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,26 +11,15 @@ module.exports = {
         availableGray: "#D9D9D9",
         themegray: "#F4F4F4",
         spacegray: "#41464b",
-        bordergray: '#A0A6AC',
-        tabsgray: '#C6C7C8',
-<<<<<<< HEAD
-        bgbutton: '#842029',
-        
-        /**market listing color  */
-
-        availableGray: '#D9D9D9',
-        themegray: '#F4F4F4',
-        spacegray: '#41464b',
-=======
->>>>>>> 94161c5278d1813eb82b8e3b6cb86094ba641357
-        typegray: '#6C757D',
+        bordergray: "#A0A6AC",
+        tabsgray: "#C6C7C8",
+        bgbutton: "#842029",
+        typegray: "#6C757D",
         primary: "#41464B",
         secondary: "#000000",
         bgprimary: "#D9D9D9",
         bgsecondary: "#6C757D",
-
       },
-
     },
   },
   plugins: [],
