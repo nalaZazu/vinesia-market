7a6bfe9fd94bf1ab07779bf54cbe4d0848292866
tauqeer180 <tauqeermalik855@gmail.com>
2023-11-02 10:39:05 +0500

    Slider Fix

--- src/components/heritage/page.tsx
+++ src/components/heritage/page.tsx
@@ -63,85 +63,19 @@ const Heritage = () => {
           </div>   */}
 
           <div className="  grid lg:grid-cols-2 md:grid-cols-1 grid-cols-2 gap-4 md:gap-0">
-            <div className="lg:mb-0 mb-6">
-              <div className="lg:grid lg:grid-cols-2 md:grid-cols-1 grid-cols-2 lg:gap-8 md:gap-0">
-                <div>
-                  <Image
-                    src={winbottle}
-                    alt="Picture of the author"
-                    className="mb-3"
-                  />
-                  <h4 className=" font-semibold  text-base	leading-6">
-                    Brunello di Montalcino "Piaggione" | 2019
-                  </h4>
-                  <p className="flex text-xs  items-center font-medium">
-                    6 bottles collection{" "}
-                    <span className="ml-2">
-                      <Image
-                        src={alert}
-                        alt="Picture of the author"
-                        width={15}
-                        height={15}
-                        quality={75}
-                      />
-                    </span>
-                  </p>
-                  <span>
-                    <p className="font-semibold text-xs">Owner</p>
-                    <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
-                      Vinesia
-                    </span>
-                  </span>
-                  <hr className="my-4 text-[#C6C7C8] font-bold" />
-                  <span>
-                    <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
-                    <h3 className="font-semibold text-lg">€3,600</h3>
-                  </span>
-                  <span className="flex font-medium text-xs">
-                    <p>50 bottles</p>
-                    <p className="text-[#959596] ml-3">50 remaining </p>
-                  </span>
-                </div>
-                <div>
-                  <Image
-                    src={winbottle}
-                    alt="Picture of the author"
-                    className="mb-3"
-                  />
-                  <h4 className=" font-semibold  text-base	leading-6">
-                    Brunello di Montalcino "Piaggione" | 2019
-                  </h4>
-                  <p className="flex text-xs  items-center font-medium">
-                    6 bottles collection{" "}
-                    <span className="ml-2">
-                      <Image
-                        src={alert}
-                        alt="Picture of the author"
-                        width={15}
-                        height={15}
-                        quality={75}
-                      />
-                    </span>
-                  </p>
-                  <span>
-                    <p className="font-semibold text-xs">Owner</p>
-                    <span className="inline-flex items-center rounded-md bg-[#842029] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-[#842029]">
-                      Vinesia
-                    </span>
-                  </span>
-                  <hr className="my-4 text-[#C6C7C8] font-bold" />
-                  <span>
-                    <h3 className="font-semibold text-xs leading-4">BUY NOW</h3>
-                    <h3 className="font-semibold text-lg">€3,600</h3>
-                  </span>
-                  <span className="flex font-medium text-xs">
-                    <p>50 bottles</p>
-                    <p className="text-[#959596] ml-3">50 remaining </p>
-                  </span>
-                </div>
+            <div>
+              <div>
+                <Slider {...sliderSettings}>
+                  {[1, 2, 3, 4]?.map((p, i) => {
+                    return (
+                      <div key={i}>
+                        <ProductCard />
+                      </div>
+                    );
+                  })}
+                </Slider>
               </div>
             </div>
-
             <div className="ml-4  lg:mt-0">
               <Image
                 src={backbg}
