f26876dca017dfefe92b918768e684d104ebf220
uswanadeem09675 <wtsplayer001@gamil.com>
2023-11-09 15:19:24 +0500

    done-all-page

---
+++ src/app/wineries/[winaerregion]/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import DropDown from "@/components/dropdown/page";
+import {
+  artCollect,
+  bottleSize,
+  casing,
+  color,
+  other,
+  ratingCritics,
+  regions,
+  winary,
+} from "@/constants/invesdropdown";
+import Recomend from "@/components/dropdown/recomend/page";
+import { productlist } from "@/constants/winelist";
+import RegionCountry from "@/components/regioncountry/page";
+import resoImage from "@/assets/image/reso-image.png";
+import ExploreCountry from "@/components/explorecountry/page";
+import { usePathname } from "next/navigation";
+
+const WinerRegion = ({ params }: { params: any }) => {
+  console.log("param", params);
+  const { winaerregion } = params;
+  const pathname = usePathname();
+  const regionparagraph = `Not all great wine producers come from traditional families or have a long history in winemaking. A notable example is Jérôme Galeyrand, whose journey in the world of wine began when he purchased his first half-hectare vineyard in 2002.`;
+  const regionparagraphs = `Up until then, Jérôme Galeyrand worked in the food industry, and his first immersion into the Burgundy world happened during two harvests at Domaine Alain Burguet in Gevrey-Chambertin.
+  `;
+
+  const explorParagraph = ` This encounter sparked an overwhelming passion for wine in him, leading him to enroll in the renowned wine school in Beaune and decide to completely change direction.`;
+  const explorParagraphs = `
+  Despite not owning vineyards in the prestigious terroirs of Grand Cru or Premier Cru in Burgundy, Jérôme Galeyrand produces regional-level wines that surprisingly managed to break the barriers of Burgundy’s appellation system, achieving a quality and recognition far beyond expectations.`;
+  const winaryOwner=`Winery owner`;
+  const authorName = `Yann Charlopin`;
+  const explor1 = `The cultivation of Jérôme Galeyrand’s vineyards is based on sustainable organic practices. Additionally, some biodynamic techniques are applied, seeking harmony between nature and wine. This careful and respectful approach to the environment results in grapes of exceptional quality.`;
+  const explor2 = `Jérôme Galeyrand is a true phenomenon in the Burgundy region, earning recognition and acclaim for his tireless dedication to producing exceptional wines. Each glass of his wines reveals the passionate soul of a producer who challenges conventions and exceeds all expectations.`;
+  return (
+    <>
+      <div className="container mx-auto  pt-3 md:pt-5  px-4 flex gap-4">
+        <p className="text-xxs font-normal text-breadcrumb tracking-wide">
+          Vinesia Marketplace
+        </p>
+        <p className="text-xxs font-normal text-breadcrumb tracking-wide">
+          / Home Page
+        </p>
+        <p className="text-xxs font-normal text-breadcrumb tracking-wide">
+          / Winery
+        </p>
+        <p className="text-bgtertiary text-xxs font-normal  tracking-wide">
+          / {winaerregion}
+        </p>
+      </div>
+      <div className="container mx-auto pt-3 md:pt-5 lg:pt-10 pb-7 px-4 ">
+        <h1 className="text-primary text-xxl font-semibold  tracking-tight">
+          {winaerregion}
+        </h1>
+        <RegionCountry
+          regionparagraph={regionparagraph}
+          regionparagraphs={regionparagraphs}
+          image={resoImage}
+          winaryOwner={winaryOwner}
+          authorName={authorName}
+        />
+
+        <ExploreCountry
+          explorParagraph={explorParagraph}
+          explorParagraphs={explorParagraphs}
+          explor1={explor1}
+          explor2={explor2}
+        />
+
+        {/* dropdown  */}
+        <div className="flex justify-between md:pt-7 md:pb-7 flex-wrap">
+          <div className="flex gap-2 flex-wrap">
+            <div>
+              <DropDown option={bottleSize} />
+            </div>
+            <div>
+              <DropDown />
+            </div>
+            <div>
+              <DropDown option={casing} />
+            </div>
+            <div>
+              <DropDown option={ratingCritics} />
+            </div>
+            <div>
+              <DropDown option={regions} />
+            </div>
+            <div>
+              <DropDown option={winary} />
+            </div>
+            <div>
+              <DropDown />
+            </div>
+            <div>
+              <DropDown option={color} />
+            </div>
+            <div>
+              <DropDown />
+            </div>
+            <div>
+              <DropDown option={artCollect} />
+            </div>
+            <div>
+              <DropDown option={other} />
+            </div>
+          </div>
+          <div className="flex items-center gap-2 pe-2 flex-wrap">
+            <p className="text-primary text-xs font-normal  tracking-wide">
+              Sort by
+            </p>
+            <Recomend />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {productlist?.map((item) => {
+            const {
+              bottle1,
+              id,
+              imageAlt,
+              imageSrc,
+              name,
+              price,
+              remain,
+              subtitle,
+            } = item;
+            return (
+              <div key={id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md ">
+                  <Image
+                    src={imageSrc}
+                    alt={imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className=" text-left">
+                  <h4 className=" text-base font-semibold">{name}</h4>
+                  <p className=" text-xxs ">{subtitle}</p>
+                  <p className=" text-xxs font-semibold">OWNER</p>
+                  <span className="justify-center items-center gap-0.5 inline-flex  mb-2  text-white text-xxs">
+                    <p className="   bg-bgbutton w-12 text-center   p-0.5 rounded-sm">
+                      Vinesia
+                    </p>
+                    <p className="  bg-[#6c757e] font-medium tracking-wide p-0.5 rounded-sm">
+                      Private investors
+                    </p>
+                  </span>
+
+                  <hr />
+                  <button className="text-xxs uppercase ">buy now</button>
+                  <h3 className=" font-semibold text-lg ">€{price}</h3>
+                  <p className=" flex gap-3">
+                    <span className=" text-xxs font-medium">{bottle1}</span>
+                    <span className=" text-xxs">{remain} remaining</span>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WinerRegion;
--- src/components/exploreRegion/page.tsx
+++ src/components/exploreRegion/page.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
 import React from "react";
+import play from "@/assets/icon/Play_Circle.svg";
+import arrow from "@/assets/icon/arrow-right2.svg";
+import Link from "next/link";
 const ExploreRegion = () => {
   return (
     <div>
-      <section className=" container mx-auto">
+      <section className="container mx-auto">
         <div className="py-10">
           <h1 className="text-primary font-semibold  tracking-tight text-xxl">
             Explore by region
@@ -10,39 +14,168 @@ const ExploreRegion = () => {
         </div>
         <div className=" grid lg:grid-cols-6 gap-4">
           {/* column one */}
-          <div className="border border-bordergray h-80 items-end justify-left p-3">
+          <div className="border border-[#7c7b83] h-[21rem] items-end justify-left p-3">
             <div className="flex items-end justify-center h-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-bordergray"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
-                />
-              </svg>
+              <Image src={play} alt="play-button" />
             </div>
             <div className=" flex items-end justify-left h-1/2">
-              <h1 className=" font-semibold text-3xl ">France</h1>
+              <h1 className=" text-black text-xxl font-semibold tracking-tight">
+                France
+              </h1>
             </div>
           </div>
+
           {/* column two */}
-          <div className="border border-bordergray h-80 items-end justify-left p-3">
-            <div className="flex items-end justify-center h-1/2"></div>
-            <div className=" flex items-end justify-left h-1/2">
-              <h1 className=" font-semibold text-3xl ">France</h1>
+
+          <div className=" h-[21rem] items-end justify-left p-3">
+            <div>
+              <ul className="text-secondary text-sm font-normal   leading-[49px] tracking-tight">
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Bordeaux</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Burgundy</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Champagne</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Rhône</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Alsace</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+              </ul>
             </div>
           </div>
+
+          {/* column two */}
+          <div className="col-span-2 flex flex-col gap-4">
+            <div className="border  border-[#7c7b83] h-40 items-end justify-left p-3 ">
+              <div className="flex items-end justify-center h-1/2">
+                <Image src={play} alt="play-button" />
+              </div>
+              <div className=" flex items-end justify-left h-1/2">
+                <h1 className="text-black text-xxl font-semibold  tracking-tight ">
+                  Germany
+                </h1>
+              </div>
+            </div>
+            <div className="border  border-[#7c7b83] h-40 items-end justify-left p-3  ">
+              <div className="flex items-end justify-center h-1/2">
+                <Image src={play} alt="play-button" />
+              </div>
+              <div className=" flex items-end justify-left h-1/2">
+                <h1 className="text-black text-xxl font-semibold  tracking-tight ">
+                  Switzerland
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          {/* column two */}
+          <div className="flex flex-col gap-4">
+            <div className="border  border-[#7c7b83] h-40 items-end justify-left p-3  ">
+              <div className="flex items-end justify-center h-1/2">
+                <Image src={play} alt="play-button" />
+              </div>
+              <div className=" flex items-end justify-left h-1/2">
+                <h1 className="text-black text-xxl font-semibold  tracking-tight ">
+                  Italy
+                </h1>
+              </div>
+            </div>
+
+            {/* next div */}
+            <div className="border  border-[#7c7b83] h-40 items-end justify-left p-3  ">
+              <div className="flex items-end justify-center h-1/2">
+                <Image src={play} alt="play-button" />
+              </div>
+              <div className=" flex items-end justify-left h-1/2">
+                <h1 className="text-black text-xxl font-semibold  tracking-tight ">
+                  USA
+                </h1>
+              </div>
+            </div>
+          </div>
+          {/* link colum */}
+          <div className=" items-end justify-left p-3">
+            <div>
+              <ul className="text-secondary text-sm font-normal   leading-[49px] tracking-tight">
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Tuscany</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+
+                <Link href="/">
+                  <li className="flex items-center justify-between">
+                    <span>Piedmont</span>
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </li>
+                </Link>
+              </ul>
+            </div>
+
+            <div className=" items-end justify-left p-3">
+              <div>
+                <ul className="text-secondary text-sm font-normal   leading-[49px] tracking-tight">
+                  <Link href="/">
+                    <li className="flex items-center justify-between">
+                      <span>Tuscany</span>
+                      <span>
+                        <Image src={arrow} alt="arrow" />
+                      </span>
+                    </li>
+                  </Link>
+
+                  <Link href="/">
+                    <li className="flex items-center justify-between">
+                      <span>Piedmont</span>
+                      <span>
+                        <Image src={arrow} alt="arrow" />
+                      </span>
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* next link comlum */}
         </div>
       </section>
     </div>
