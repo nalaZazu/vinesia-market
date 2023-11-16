e446909603e9bf1d9ad3969e800a1fbbf0dc1934
hsnabdullah <hsnabdullah89@gmail.com>
2023-11-16 15:49:44 +0500

    footer

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,7 +6,6 @@ import redwine from "@/assets/icon/redwine.svg";
 import BreadCrumb from "@/common/BreadCrumb";
 import PeaceOfMind from "@/components/productDetail/PeaceOfMind";
 import { getPriceHistory, getProductDetail } from "@/services/ProductDetail";
-import ReleaseDateSection from "@/components/productDetail/ReleaseDateSection";
 import PTSSkelton from "@/components/productDetail/PTSSkelton";
 import { getHomePage } from "@/services/Home";
 import AllEditions from "@/components/productDetail/AllEditions";
@@ -22,6 +21,7 @@ import WineryVideo from "@/components/productDetail/WineryVideo";
 import AboutWineries from "@/components/productDetail/AboutWineries";
 import PricingDetail from "@/components/productDetail/PricingDetails";
 import Appactivity from "@/components/charts/page";
+import Footer from "@/components/footer/page";
 
 // import ReleaseDateSection from '@/components/productDetail/ReleaseDateSection'
 // client componet fetching
@@ -39,9 +39,8 @@ export default function Product() {
     setIsOpen(!isOpen);
   };
   const [isOpen, setIsOpen] = useState(false);
- 
+
   useEffect(() => {
-   
     if (id) {
       getProductDetail(id)
         .then((res) => {
